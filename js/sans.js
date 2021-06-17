cb0b7c35041d1e72cbd4ab9f5ebcf752d19678be
Luis Frentzen <luisfrentzen@gmail.com>
2021-06-17 20:51:33 +0700

    added sans movement

--- js/sans.js
+++ js/sans.js
@@ -9,43 +9,71 @@ class Sans {
     this.body = document.getElementById("sans-torso");
     this.head = document.getElementById("sans-head");
     this.leg = document.getElementById("sans-legs");
+    this.headGap = 6;
 
-    this.scale = 1.5;
+    this.scale = 2;
     this.width = this.body.width * this.scale;
     this.height =
-      this.head.height * this.scale +
-      this.body.height * this.scale +
-      this.leg.height * this.scale -
-      8;
+      (this.head.height + this.body.height + this.leg.height - this.headGap) *
+      this.scale;
+
+    this.bodyMovementX = 0;
+    this.bodyMovementY = 0;
+    this.bodyMoveSpeedX = 0.015 * this.scale;
+    this.bodyMoveSpeedY = 0.015 * this.scale;
+
+    this.headMovementX = 0;
+    this.headMovementY = 0;
+    this.headMoveSpeedX = 0.015 * this.scale;
+    this.headMoveSpeedY = 0.017 * this.scale;
+  }
+
+  update() {
+    this.bodyMovementX += this.bodyMoveSpeedX;
+    this.bodyMovementY += this.bodyMoveSpeedY;
+
+    if (this.bodyMovementX > 0.62 * this.scale || this.bodyMovementX < 0) {
+      this.bodyMoveSpeedX *= -1;
+    }
+    if (this.bodyMovementY < -0.3 * this.scale || this.bodyMovementY > 0) {
+      this.bodyMoveSpeedY *= -1;
+    }
+
+    this.headMovementX += this.headMoveSpeedX;
+    this.headMovementY += this.headMoveSpeedY;
+
+    if (this.headMovementX > 0.62 * this.scale || this.headMovementX < 0) {
+      this.headMoveSpeedX *= -1;
+    }
+    if (this.headMovementY < -0.36 * this.scale || this.headMovementY > 0) {
+      this.headMoveSpeedY *= -1;
+    }
   }
 
   render(ctx) {
+    this.update();
     ctx.drawImage(
       this.body,
-      this.x,
-      this.y + this.head.height * this.scale - 8,
+      this.x + this.bodyMovementX * this.scale,
+      this.y +
+        (this.head.height - this.headGap + this.bodyMovementY) * this.scale,
       this.body.width * this.scale,
-      this.body.height * 1.5
+      this.body.height * this.scale
     );
     ctx.drawImage(
       this.leg,
-      this.x +
-        (this.body.width * this.scale) / 2 -
-        (this.leg.width * this.scale) / 2 +
-        2,
+      this.x + (this.body.width / 2 - this.leg.width / 2 + 2) * this.scale,
       this.y +
-        this.body.height * this.scale +
-        this.head.height * this.scale -
-        8,
+        (this.body.height + this.head.height - this.headGap) * this.scale,
       this.leg.width * this.scale,
       this.leg.height * this.scale
     );
     ctx.drawImage(
       this.head,
       this.x +
-        (this.body.width * this.scale) / 2 -
-        (this.head.width * this.scale) / 2,
-      this.y,
+        (this.body.width / 2 - this.head.width / 2 + this.headMovementX) *
+          this.scale,
+      this.y + this.headMovementY * this.scale,
       this.head.width * this.scale,
       this.head.height * this.scale
     );
